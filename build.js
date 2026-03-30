fedaaa5b7527fbbca6ffff69f61019b10e2ebc89
Diramix <danyagranny10@proton.me>
2026-03-30 11:15:32 +0300

    fix(build.js): storePage build

--- build.js
+++ build.js
@@ -2,10 +2,17 @@ import esbuild from "esbuild";
 import fs from "fs";
 import path from "path";
 import { minify } from "html-minifier-terser";
+import { minify as terserMinify } from "terser";
 import * as lightningcss from "lightningcss";
 
 const SRC = "src";
 const DIST = "dist";
+
+const BROWSER_DIRS = [path.join(SRC, "lib", "storePage", "public")];
+
+function isBrowserFile(file) {
+    return BROWSER_DIRS.some((dir) => file.startsWith(dir));
+}
 
 function walk(dir, out = []) {
     for (const file of fs.readdirSync(dir)) {
@@ -26,12 +33,31 @@ async function build() {
         fs.mkdirSync(path.dirname(outFile), { recursive: true });
         const ext = path.extname(file);
         if (ext === ".js") {
+            if (isBrowserFile(file)) {
+                const code = fs.readFileSync(file, "utf8");
+                const result = await terserMinify(code, {
+                    ecma: 2020,
+                    compress: true,
+                    mangle: true,
+                });
+                fs.writeFileSync(outFile, result.code);
+            } else {
+                esbuild.buildSync({
+                    entryPoints: [file],
+                    outfile: outFile,
+                    bundle: false,
+                    platform: "node",
+                    format: "esm",
+                    minify: true,
+                });
+            }
+        } else if (ext === ".cjs") {
             esbuild.buildSync({
                 entryPoints: [file],
                 outfile: outFile,
                 bundle: false,
                 platform: "node",
-                format: "esm",
+                format: "cjs",
                 minify: true,
             });
         } else if (ext === ".css") {
@@ -50,7 +76,7 @@ async function build() {
                 removeRedundantAttributes: true,
                 removeEmptyAttributes: true,
                 minifyCSS: true,
-                minifyJS: true,
+                minifyJS: false,
             });
             fs.writeFileSync(outFile, minified);
         } else {
